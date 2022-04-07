800a4537623ba6c92bcc0b0406ad0bbba83665a3
Sylvia-Chepkoech <slycheche@gmail.com>
2022-04-07 23:07:27 +0300

    add changes to the goal app

---
+++ src/app/goaldetail/goaldetail.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Goal } from '../goal';
+
+@Component({
+  selector: 'app-goaldetail',
+  templateUrl: './goaldetail.component.html',
+  styleUrls: ['./goaldetail.component.css']
+})
+export class GoaldetailComponent implements OnInit {
+  @Input() goal: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
